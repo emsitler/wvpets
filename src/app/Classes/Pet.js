ac04a454b8b13e0434a136d46a2cfcdc116f8c30
emsitler <ems00030@mix.wvu.edu>
2023-03-06 14:07:28 -0500

    Added Pet.js, updated layouts
    layouts subject to further change, img backgrounds look bad

---
+++ src/app/Classes/Pet.js
@@ -0,0 +1,29 @@
+class Pet {
+    constructor(name, type) {
+        this.name = name;
+        switch(type) {
+            case 1:
+                this.image = "..."
+                this.animal = "Raccoon"
+                break;
+            case 2:
+                this.image = "..."
+                this.animal = "Bear"
+                break;
+            case 3:
+                this.image = "..."
+                this.animal = "Blue Jay"
+                break;
+            case 4:
+                this.image = "..."
+                this.animal = "Bobcat"
+                break;
+            default:
+                this.image = "..."
+                this.animal = "Raccoon"
+                break;
+        }
+    }
+
+
+}
